9c387506297840bd7c27b68b3fc6aad6f6b0b116
byverdu <byverdu@gmail.com>
2014-09-29 20:35:02 +0100

    starting from scratch for practice

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,37 +1,6 @@
 class RestaurantsController < ApplicationController
 
-	def index
-		@restaurants = Restaurant.all
-	end
 
-	def new
-		@restaurant = Restaurant.new
-	end
-
-	def create
-		restaurant = Restaurant.create(params[:restaurant].permit(:name))
-
-		redirect_to restaurants_path
-	end
-
-	def edit
-    @restaurant = Restaurant.find(params[:id])
-	end
-
-	def update
-		@restaurant = Restaurant.find(params[:id])
-		@restaurant.update(params[:restaurant].permit(:name))
-
-		redirect_to restaurants_path
-	end
-
-	def destroy
-		@restaurant = Restaurant.find(params[:id])
-		@restaurant.destroy
-
-		flash[:notice] = 'Restaurant deleted'
-		redirect_to restaurants_path
-	end
 
 
 end
--- spec/features/restaurants_spec.rb
+++ spec/features/restaurants_spec.rb
@@ -1,73 +1,7 @@
 require "rails_helper"
 
 describe "Restaurant" do
-	context 'Main page' do
-		it "has no restaurant" do
-			visit '/restaurants'
-
-			expect(page).to have_content('There is no restaurants')
-			expect(page).to have_link('Add restaurant')
-		end
-	end
-
-	context "Adding a restaurant" do
-		before do
-		  Restaurant.create(name: 'Nero')
-		end
-
-		it "displays the restaurants" do
-			visit '/restaurants'
-
-			expect(page).to     have_content('Nero')
-			expect(page).not_to have_content('There is no restaurants')
-		end
-
-		it "can add a restaurant" do
-		  visit '/restaurants'
-
-		  click_link 'Add restaurant'
-
-			fill_in 'Name', with: 'Nero' 			
-			click_button 'Create Restaurant'
-			
-			expect(page).to have_content('Nero')
-			expect(current_path).to eq '/restaurants'
-		end
-	end
-
-	context 'Editing the restaurants' do
-		before do
-		  Restaurant.create(name: 'Nero')
-		end
-
-		it "the user can edit restaurants info" do
-			visit '/restaurants'
-
-			click_link 'Edit Nero'
-			fill_in 'Name', with: 'Cafe Nero'
-			click_button 'Update Restaurant'
-
-			expect(page).to     have_content('Cafe Nero')
-			expect(current_path).to eq '/restaurants'
-		end
-	end
-
-	context "Deleting a restaurant" do
-		before do 
-			Restaurant.create(name: 'Nero')
-		end
-
-		it "the user can delete a restaurant by clicking a button" do
-			visit '/restaurants'
-
-			click_link 'Delete'
-
-			expect(page).not_to have_content('Nero')
-			expect(page).to     have_content('Restaurant deleted')
-			expect(current_path).to eq '/restaurants'
-			
-		end
-	end
+	
 
 end
 
