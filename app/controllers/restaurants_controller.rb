94883d75159cf805905db930bd9c82a7de7a6d2e
byverdu <byverdu@gmail.com>
2014-09-29 21:06:47 +0100

    starting page tested

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,8 @@
 class RestaurantsController < ApplicationController
 
-
+def index
+	
+end
 
 
 end
--- spec/features/restaurants_spec.rb
+++ spec/features/restaurants_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 describe "Restaurant" do
-	
+	context 'When visiting the page for first time' do
+		it "There is no restaurants" do
+			visit '/restaurants'
+
+			expect(page).to have_content('YELP')
+			expect(page).to have_content('There is no Restaurants')
+			expect(page).to have_content('Add Restaurant')
+		end
+	end
 
 end
 
