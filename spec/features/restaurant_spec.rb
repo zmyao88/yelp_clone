2093cd0362cde1ee9245d6aca476457fd4cfbb4c
byverdu <byverdu@gmail.com>
2014-09-30 12:53:19 +0100

    displaying restaurants, tested

--- spec/features/restaurant_spec.rb
+++ spec/features/restaurant_spec.rb
@@ -13,7 +13,7 @@ describe "Restaurant" do
 
   	context 'When a restaurant has been added' do
   		before do
-        Restaurant.create(name: 'Nero')
+        Restaurant.create(name: 'Nero', cuisine: 'Cafe', description: 'Italian coffee')
   		end
 
   		it "The user will see all the restaurants" do
@@ -21,7 +21,18 @@ describe "Restaurant" do
 
   			expect(page).to     have_content('YELP')
   			expect(page).to     have_content('Nero')
+  			expect(page).to     have_content('Cafe')
+  			expect(page).to     have_content('Italian coffee')
   			expect(page).not_to have_content('There is no restaurants yet')
+  		end
+
+  		xit "The user can add a restaurant" do
+  			visit '/restaurants'
+
+  			click_link 'Add Restaurant'
+  			fill_in 'Name', with:
+
+        
   		end
   	end
 
