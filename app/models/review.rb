9d2913766020c9eb8208e84c3ac32566c40fa619
byverdu <byverdu@gmail.com>
2014-10-01 23:42:42 +0100

    added endorsement model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
-	belongs_to :restaurant 
+	belongs_to :restaurant
+	has_many   :endorsements 
   validates  :thoughts, length: {minimum: 1}
   validates  :rating,   inclusion: (1..5)
 end
