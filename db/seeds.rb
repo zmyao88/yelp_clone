f74430154c6c5a76aee345f4abdb4aa876e41e51
byverdu <byverdu@gmail.com>
2014-10-01 22:59:36 +0100

    added seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.destroy_all
+Review.destroy_all
 
-# Restaurant.destroy_all
 
-# kfc = Restaurant.create(name: 'KFC' )
-# kfc.reviews.create(rating:1, thoughts:'bad')
-# kfc.reviews.create(rating:2, thoughts:'so so ')
-# kfc.reviews.create(rating:3, thoughts:'too fried')
-# kfc.reviews.create(rating:4, thoughts:'bad')
-# kfc.reviews.create(rating:5, thoughts:'great')
-# kfc.reviews.create(rating:3, thoughts:'bad')
+kfc = Restaurant.create(name: 'KFC',description: 'Kentucky Fried Chicken', cuisine: 'Fast Food' )
+kfc.reviews.create(rating:1, thoughts:'bad')
+kfc.reviews.create(rating:2, thoughts:'so so ')
+kfc.reviews.create(rating:3, thoughts:'too fried')
+kfc.reviews.create(rating:4, thoughts:'bad')
+kfc.reviews.create(rating:5, thoughts:'great')
+kfc.reviews.create(rating:3, thoughts:'bad')
 
-# jamie = Restaurant.create(name: 'Jamie´s' )
-# jamie.reviews.create(rating:1, thoughts:'bad')
-# jamie.reviews.create(rating:2, thoughts:'so so ')
-# jamie.reviews.create(rating:3, thoughts:'too fried')
-# jamie.reviews.create(rating:4, thoughts:'bad')
-# jamie.reviews.create(rating:5, thoughts:'great')
-# jamie.reviews.create(rating:3, thoughts:'')
+
+jamie = Restaurant.create(name: 'Jamie´s', description: 'Italian Food', cuisine: 'Mediterranean'  )
+jamie.reviews.create(rating:1, thoughts:'bad')
+jamie.reviews.create(rating:2, thoughts:'so so ')
+jamie.reviews.create(rating:3, thoughts:'too fried')
+jamie.reviews.create(rating:4, thoughts:'bad')
+jamie.reviews.create(rating:5, thoughts:'great')
+jamie.reviews.create(rating:3, thoughts:'')
+
