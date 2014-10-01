54e3f8f901fca45ff10a79b3e8e604c67afffac1
byverdu <byverdu@gmail.com>
2014-10-01 20:37:57 +0100

    rating with 5 stars tested

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,7 +1,9 @@
 module ReviewsHelper
 
 	def star_rating rating
-		rating
+		return rating unless rating.is_a?(Fixnum)
+
+		'★' * rating 
 	end
 
 end
--- spec/helpers/review_helpers_spec.rb
+++ spec/helpers/review_helpers_spec.rb
@@ -6,5 +6,9 @@ describe ReviewsHelper, :type => :helper do
 		it 'does nothing for not a number' do
 		  expect(helper.star_rating('N/A')).to eq('N/A')
 		end
+
+		it "returns 5 black stars for 5" do
+			expect(helper.star_rating(5)).to eq("★★★★★")
+		end
 	end
 end
