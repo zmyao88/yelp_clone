60bff811662e050d148e7018576918cde826d97e
byverdu <byverdu@gmail.com>
2014-10-06 18:13:23 +0100

    changed root path to point /restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :endorsements
   end
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'restaurants#index'
 
 
   # Example of regular route:
