f314aa3602b99e4ffc8b867b80b764dbadbba3be
byverdu <byverdu@gmail.com>
2014-10-08 22:10:52 +0100

    the user name is unique, tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username, presence:true, uniqueness: { case_sentive: false }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, 
          :validatable, :authentication_keys => [:username]
 
-  validates :username, length: { minimum: 1 }
 
    attr_accessor :login
 
--- spec/models/user_models_spec.rb
+++ spec/models/user_models_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
+	before do 
+		create :byverdu
+	end
+
 	it "The user name field must be filled" do
 		
 		user = User.new(username: '')
 		expect(user).to have(1).error_on(:username)
 		expect(user).not_to be_valid 
 	end
+
+	it "The username must be unique" do
+    user = User.new(username: 'byverdu')
+
+		expect(user).to have(1).error_on(:username)
+	end
 end
+
