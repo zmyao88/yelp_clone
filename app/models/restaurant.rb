af78a2eaecf573c2b9eb0bb8db8d1a882158e823
byverdu <byverdu@gmail.com>
2014-10-08 14:55:32 +0100

    sign in form styled

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -16,6 +16,14 @@ has_attached_file :image,
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 
+#   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+# :default_url => "/images/:style/missing.png",
+#                     :storage => :s3,
+#                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials},
+#                     :url =>':s3_domain_url',
+#     :path => '/:class/:attachment/:id_partition/:style/:filename'
+
+  
 
   def s3_credentials
     {:bucket => "byverdu-yelp", :access_key_id => Rails.application.secrets[:access_key_id], :secret_access_key => Rails.application.secrets[:secret_access_key]}
